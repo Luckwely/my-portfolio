fbb354fd1ec6d9b24096613e4d754c7b54c62a99
Jean Luc RANDRIAMIHARISOA <vst.pascal@mmv.madamvalue.mg>
2026-06-11 22:49:01 +0200

    style: remove leftover merge conflict markers

--- src/main.js
+++ src/main.js
@@ -10,10 +10,7 @@ app.use(PrimeVue, {
     theme: {
         preset: Aura,
         options: {
-<<<<<<< HEAD
-=======
             darkModeSelector: 'none',
->>>>>>> eb46e54 (1st commit)
             cssLayer: {
                 name: 'primevue',
                 order: 'tailwind-base, primevue, tailwind-utilities'
